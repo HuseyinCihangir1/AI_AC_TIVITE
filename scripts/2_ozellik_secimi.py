4834471ea39833a513883fdff09de498904d744a
HuseyinCihangir1 <hyccihangir@gmail.com>
2026-05-15 19:57:43 +0300

    Refactor: huseyin cihangir proje final hali

--- scripts/2_ozellik_secimi.py
+++ scripts/2_ozellik_secimi.py
@@ -1,23 +1,37 @@
 import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.feature_selection import SelectKBest, f_classif
 from sklearn.ensemble import RandomForestClassifier
 
-# processed veriyi yükleme:
+# 1. processed Veriyi Yükleme
 train = pd.read_csv('../data/train_cleaned.csv')
 X = train.drop(['Activity', 'subject'], axis=1)
 y = train['Activity']
 
-# 1. Normalizasyon (Zorunlu) kısmı:
-scaler = MinMaxScaler()
-X_scaled = scaler.fit_transform(X)
+# ÖZELLİK SEÇİMİ:
 
-# 2. Ozellik Seçme Yöntem 1: Select KBest yaptık:
-selector = SelectKBest(score_func=f_classif, k=50)
-X_new = selector.fit_transform(X_scaled, y)
+# Adım 1: Normalizasyon 
+scaler = MinMaxScaler() #veriyi 0-1 arasına kısıtlıyoruz
+X_scaled = pd.DataFrame(scaler.fit_transform(X), columns=X.columns)
 
-# 3. Özellik Seçimi Yöntem 2: Random Forest uyguladik:
-rf = RandomForestClassifier()
+# YÖNTEM 1: İstatistiksel Seçim(KBest)
+selector = SelectKBest(score_func=f_classif, k=50) #En yüksek f-skoruna sahip 50 özelliği seçicez
+X_best = selector.fit_transform(X_scaled, y)
+
+# YÖNTEM 2: Model Tabanlı Seçim(Random Forest)
+rf = RandomForestClassifier(n_estimators=100, random_state=42)#Tüm Özelliklerin sınıflandırmadaki etkisini bulur
 rf.fit(X_scaled, y)
+importances = pd.Series(rf.feature_importances_, index=X.columns).sort_values(ascending=False)
 
-print("2 gorevde tamamlandı: Normalizasyon ve 3 farklı özellik seçimi uygulandı.")
+
+# 3-YÖNTEM : Korelasyon Tabanlı Seçim
+plt.figure(figsize=(12,10))# Birbiriyle aşırı benzer  olan özellikleri tespit edicek kısım
+cor = X_scaled.iloc[:, :20].corr() # İlk 20 özellik için ısı haritasi cikaricaz
+sns.heatmap(cor, annot=True, cmap=plt.cm.Reds)
+plt.title("Özellikler Arası Korelasyon Isı Haritası")
+plt.show() # Bu grafiğin ekran görüntüsü(sunum için ss alınacak*)
+
+print("Görev 2 Tamamlandı: Normalizasyon ve 3 farklı özellik seçimi uygulandı.")
