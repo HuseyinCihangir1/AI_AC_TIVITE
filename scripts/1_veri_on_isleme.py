ccc22d8948f7fda850036fa7334169decc118d56
HuseyinCihangir1 <hyccihangir@gmail.com>
2026-05-15 16:28:22 +0300

    Proje Yapisi eklendi

---
+++ scripts/1_veri_on_isleme.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+from scipy import stats
+
+# Veri Yükleme
+train = pd.read_csv('../data/train.csv')
+test = pd.read_csv('../data/test.csv')
+
+# Görev 1: Aykırı Değer Temizliği (Z-Score)
+# Sadece sayısal sütunlarda aykırı değer kontrolü yapıyoruz
+X_train = train.drop(['Activity', 'subject'], axis=1)
+z_scores = stats.zscore(X_train)
+abs_z_scores = np.abs(z_scores)
+filtered_entries = (abs_z_scores < 3).all(axis=1)
+
+train_cleaned = train[filtered_entries]
+train_cleaned.to_csv('../data/train_cleaned.csv', index=False)
+
+print("Görev 1 Tamamlandı: Aykırı değerler temizlendi ve veri kaydedildi.")
